5a584a5d98a36f5af4d9a242155e6ae339b476ef
aslan.gama <aslan.gama@sixchains.com.br>
2024-02-15 15:18:37 -0300

    nuxt-dropzone

--- src/module.ts
+++ src/module.ts
@@ -7,12 +7,27 @@ import {
     addRouteMiddleware
 } from '@nuxt/kit'
 
+interface ModuleOptions {
+
+}
 
 
-const PACKAGE_NAME:string = 'nuxt-dropzone'
+const PACKAGE_NAME: string = 'nuxt-dropzone'
 export default defineNuxtModule<ModuleOptions>({
 
     meta: {
         name: PACKAGE_NAME,
+        configKey: 'nuxtDropzone',
+        // Compatibility constraints
+        compatibility: {
+            // Semver version of supported nuxt versions
+            nuxt: '^3.0.0'
+        }
     },
+
+    setup(moduleOptions, nuxt) {
+        const logger = useLogger(PACKAGE_NAME)
+
+        const {resolve} = createResolver(import.meta.url)
+    }
 })
