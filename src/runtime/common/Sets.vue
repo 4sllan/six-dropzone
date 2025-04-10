e23d385f3d273ab132d9140a13c71563a12488a7
aslan.gama <aslan.gama@sixchains.com.br>
2025-04-10 00:20:15 -0300

    Six Dropzone v0.4.3

--- src/runtime/common/Sets.vue
+++ src/runtime/common/Sets.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {ref, watch, nextTick} from "vue";
+import {ref, onMounted, watch, nextTick} from "vue";
 import type {PropType} from 'vue';
 import {backgroundImage} from "../utils";
 
